a2a931de0be059f287d9022f9e0901de325ac707
nial0003 <nial0003@stud.kea.dk>
2024-11-22 08:41:40 +0100

    Added Member class. Added attributes for name, age, gender, address, phonenumber, membertype, and hasPaid. Added the constructor for the class. Added a method which calculates the age based on the birthday provided and currentDate

---
+++ src/main/java/Member.java
@@ -0,0 +1,33 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Member {
+    private String name;
+    private int age;
+    private String gender;
+    private String address;
+    private int phoneNumber;
+    private static int memberNumber;
+    private String memberType;
+    private boolean hasPaid;
+
+    public Member (String name, LocalDate birthday, String gender, String address, int phoneNumber, String memberType, boolean hasPaid){
+        this.name = name;
+        this.age = calculateAge(birthday);
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.memberType = memberType;
+        this.hasPaid = hasPaid;
+        memberNumber++;
+    }
+
+    //Calculates the age based on the birthday provided and subtracts it from the current date.
+    //Return year in between.
+    public int calculateAge (LocalDate birthday){
+        LocalDate todaysDate = LocalDate.now();
+        Period period = Period.between(birthday, todaysDate);
+        return period.getYears();
+    }
+
+}
