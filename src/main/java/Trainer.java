9e25384fcff9fabe71ed205af777ca356228e1a4
nial0003 <nial0003@stud.kea.dk>
2024-11-25 12:31:24 +0100

    Added the Trainer class. Made it so that it takes the members who are Athletes and adds them to the Athletes arrayList.

--- src/main/java/Trainer.java
+++ src/main/java/Trainer.java
@@ -2,14 +2,35 @@ import java.util.ArrayList;
 
 public class Trainer {
     String name;
-    ArrayList<Member> Athletes;
+    ArrayList<Athlete> athletes;
+    FileHandler fh = new FileHandler();
 
     public Trainer(String name){
         this.name = name;
-        Athletes = new ArrayList<>();
+        athletes = new ArrayList<>();
     }
 
-    public void getAthletes(){
+    //Checks if a member is an Athlete and if it is, it'll add them to the special athletes ArrayList
+    public void addAthleteToList(ArrayList<Member> members){
+        for (Member member : members){
+            if (member.getMembershipType() == MembershipType.ATHLETE){
+                String[] name = member.getName().split(",\\s");
+                athletes.add(new Athlete(name[1], name[0], member.getLd(), member.getGender(),
+                        member.getAddress(), member.getPhoneNumber(), member.getMemberShipStatus().toString(), member.getMembershipType().toString(),
+                        member.getHasPaid()));
+            }
+        }
+    }
 
+    public void setAthleteTimes(int x, double time, SwimmingDisciplines discipline){
+        athletes.get(x).setDisciplineTimes(discipline, time);
+    }
+
+    public double getAthleteTime(int x, SwimmingDisciplines discipline){
+        return athletes.get(x).getTime(discipline);
+    }
+
+    public void saveAthletesToFile(){
+        fh.saveToAthleteFile(athletes);
     }
 }
