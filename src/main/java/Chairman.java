c2e2c5985ad72881629eaa24f8d4ae33e6d8e99b
nial0003 <nial0003@stud.kea.dk>
2024-11-25 12:29:32 +0100

    Changed the chairman class so it creates a new member object first and then adds it to both the newMember arraylist and to the listOfMembers Arraylist.

--- src/main/java/Chairman.java
+++ src/main/java/Chairman.java
@@ -11,12 +11,20 @@ public class Chairman {
         newMember = new ArrayList<>();
     }
 
-    //adds a new member to the newMember Arraylist and then uploads the new Member to the file at the end of the file.
+    //adds a new member to the newMember and listOfMembers Arraylist and then uploads the new Member to the file at the end of the file.
+    //This is done so that other classes will have access to the new member via the listOfMembers arraylist before it's
+    //reloaded from the file.
     public void addMember(String firstName, String lastName, int yearBorn, int monthBorn, int dayBorn, String gender,
                           String address, int number, String membershipStatus, String membershipType, boolean hasPaid){
 
-        newMember.add(new Member(firstName, lastName, LocalDate.of(yearBorn, monthBorn, dayBorn), gender, address,
-                number,membershipStatus, membershipType,  hasPaid));
+        Member member = new Member(firstName, lastName, LocalDate.of(yearBorn, monthBorn, dayBorn), gender, address,
+                number,membershipStatus, membershipType,  hasPaid);
+        newMember.add(member);
+        listOfMembers.add(member);
         fh.saveToFile(newMember);
+    }
+
+    public ArrayList<Member> getListOfMembers() {
+        return listOfMembers;
     }
 }
