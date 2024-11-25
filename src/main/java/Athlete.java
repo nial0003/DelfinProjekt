72e8c383f016e54cdfd814ea3e5412a56a7c25ae
nial0003 <nial0003@stud.kea.dk>
2024-11-25 12:33:51 +0100

    Overrode the toCSVStyle from the superclass so that it's more inline with what we want the Athlete class to save to the file.

--- src/main/java/Athlete.java
+++ src/main/java/Athlete.java
@@ -8,6 +8,7 @@ public class Athlete extends Member {
     public Athlete(String firstName, String lastName, LocalDate birthday, String gender, String address, int phoneNumber,
                    String membershipStatus, String membershipType, boolean hasPaid) {
         super(firstName, lastName, birthday, gender, address, phoneNumber, membershipStatus, membershipType, hasPaid);
+
         this.disciplineTimes = new EnumMap<>(SwimmingDisciplines.class);
     }
 
@@ -17,7 +18,19 @@ public class Athlete extends Member {
 
     //Returns the time for the given discipline, if the given discipline doesn't have a time it'll return
     //-1. Which we can then use when deciding what times to print out in the trainer class.
-    public double getTime(SwimmingDisciplines discipline){
+    public double getTime(SwimmingDisciplines discipline) {
         return disciplineTimes.getOrDefault(discipline, -1.0);
+    }
+
+    //Overrides the toCSVStyle from the super class so that it's more inline with the format we want from this class in the file
+    @Override
+    public String toCSVStyle() {
+        String disciplineTimesString = "";
+        for (var disciplines : disciplineTimes.entrySet()) {
+            if (disciplines.getValue() != -1.0) {
+                disciplineTimesString += "," + disciplines.getKey() + ":" + disciplines.getValue();
+            }
+        }
+        return getName() + disciplineTimesString + "\n";
     }
 }
