ec8180c1e087f0defdd5e40b1449a06b37eeba55
ntvorup <nach0004@stud.kea.dk>
2024-11-25 10:36:55 +0100

    Added UI and options based on position

--- src/main/java/MembershipType.java
+++ src/main/java/MembershipType.java
@@ -1,41 +1,41 @@
 public enum MembershipType {
     JUNIOR,
     SENIOR,
-    ELDER,
-    ACTIVE,
-    PASSIVE,
-    ATHLETE,
+    PENSIONIST,
+    AKTIV,
+    PASSIV,
+    ATLET,
     HOBBY;
 
-    //Assigns their age group based on their age.
-    public static Enum<MembershipType> ageGroup(int age){
-        if (age < 18){
+    // Assigns their age group based on their age.
+    public static Enum<MembershipType> ageGroup(int age) {
+        if (age < 18) {
             return JUNIOR;
         } else if (age < 60) {
             return SENIOR;
         } else {
-            return ELDER;
+            return PENSIONIST;
         }
     }
 
-    //Assigns their status by returning an Enum type that's determined by the String input which is either "ACTIVE"
-    //or "PASSIVE.
-    public static Enum<MembershipType> membershipStatus(String membershipStatus){
-        if (membershipStatus.contentEquals("ACTIVE")){
-            return ACTIVE;
-        } else if (membershipStatus.contentEquals("PASSIVE")){
-            return PASSIVE;
+    // Assigns their status by returning an Enum type that's determined by the String input which is either "ACTIVE"
+    // or "PASSIVE.
+    public static Enum<MembershipType> membershipStatus(String membershipStatus) {
+        if (membershipStatus.toUpperCase().contentEquals("AKTIV")) {
+            return AKTIV;
+        } else if (membershipStatus.toUpperCase().contentEquals("PASSIV")) {
+            return PASSIV;
         } else {
             return null;
         }
     }
 
     // Assigns their membership type, by choosing "HOBBY" or "ATHLETE"
-    public static Enum<MembershipType> membershipType(String membershipType){
-        if (membershipType.contentEquals("HOBBY")){
+    public static Enum<MembershipType> membershipType(String membershipType) {
+        if (membershipType.toUpperCase().contentEquals("HOBBY")) {
             return HOBBY;
-        } else if (membershipType.contentEquals("ATHLETE")) {
-            return ATHLETE;
+        } else if (membershipType.toUpperCase().contentEquals("ATLET")) {
+            return ATLET;
         } else {
             return null;
         }
