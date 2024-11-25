5e761eff1a7eba9aa48be0feb47faf564604a698
nial0003 <nial0003@stud.kea.dk>
2024-11-25 12:25:58 +0100

    Added the ability to save and load information to the Athletes file

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class FileHandler {
     private File file = new File("Members");
+    private File atheleteFile = new File("Athletes");
 
     //Loads the Members from the file and returns them in an ArrayList which can then be updated and used.
     public ArrayList<Member> loadFromFile() {
@@ -15,22 +16,22 @@ public class FileHandler {
 
             String line = br.readLine();
             while (line != null && !line.isEmpty()) {
-                    String[] data = line.split(",");
-                    String lastName = data[0];
-                    String firstName = data[1];
-                    int yearBorn = Integer.parseInt(data[2]);
-                    int monthBorn = Integer.parseInt(data[3]);
-                    int dayBorn = Integer.parseInt(data[4]);
-                    String gender = data[5];
-                    String address = data[6];
-                    int number = Integer.parseInt(data[7]);
-                    String membershipStatus = data[8];
-                    String membershipType = data[9];
-                    boolean hasPaid = Boolean.parseBoolean(data[10]);
+                String[] data = line.split(",");
+                String lastName = data[0];
+                String firstName = data[1];
+                int yearBorn = Integer.parseInt(data[2]);
+                int monthBorn = Integer.parseInt(data[3]);
+                int dayBorn = Integer.parseInt(data[4]);
+                String gender = data[5];
+                String address = data[6];
+                int number = Integer.parseInt(data[7]);
+                String membershipStatus = data[8];
+                String membershipType = data[9];
+                boolean hasPaid = Boolean.parseBoolean(data[10]);
 
-                    listOfMembers.add(new Member(firstName, lastName, LocalDate.of(yearBorn, monthBorn, dayBorn), gender, address,
-                            number, membershipStatus, membershipType,  hasPaid));
-                    line = br.readLine();
+                listOfMembers.add(new Member(firstName, lastName, LocalDate.of(yearBorn, monthBorn, dayBorn), gender, address,
+                        number, membershipStatus, membershipType, hasPaid));
+                line = br.readLine();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -40,14 +41,44 @@ public class FileHandler {
 
 
     //Saves list of members to the file, by converting member details to CSV-style string.
-    public void saveToFile(ArrayList<Member> listOfMembers){
-        try (FileWriter fw = new FileWriter(file, true)){
-            for (Member member : listOfMembers){
+    public void saveToFile(ArrayList<Member> listOfMembers) {
+        try (FileWriter fw = new FileWriter(file, true)) {
+            for (Member member : listOfMembers) {
                 fw.write(member.toCSVStyle());
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void saveToAthleteFile(ArrayList<Athlete> listOfAthletes) {
+        try (FileWriter fw = new FileWriter(atheleteFile)) {
+            for (Athlete athlete : listOfAthletes) {
+                fw.write(athlete.toCSVStyle());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String printFromAthleteFile() {
+        String output = "";
+        try (FileReader fr = new FileReader(atheleteFile)) {
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            while (line != null && !line.isEmpty()) {
+                String[] data = line.split(",");
+                for (int i = 0; i<data.length;i++){
+                    output += data[i] + " ";
+                }
+                output += "\n";
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return output;
     }
 }
 
