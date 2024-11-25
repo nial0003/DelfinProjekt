4a2fd85471bd8f19b2cf3fae13168becc561d696
nial0003 <nial0003@stud.kea.dk>
2024-11-25 09:20:54 +0100

    Added the trainer class which takes in a name and create an arrayList of Athletes.

---
+++ src/main/java/Trainer.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Trainer {
+    String name;
+    ArrayList<Member> Athletes;
+
+    public Trainer(String name){
+        this.name = name;
+        Athletes = new ArrayList<>();
+    }
+
+    public void getAthletes(){
+
+    }
+}
