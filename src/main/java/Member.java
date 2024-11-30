250f6e4b451f0e7e10805d477cae4a0cf863302d
Daniel-Platz <dani0005@stud.kea.dk>
2024-11-30 22:22:21 +0100

    Fixed memberNumber incrementer, by overloading constructor for members loaded from file

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 import java.time.Period;
 
-//TODO
+//TODO fixed!!!!!!!!
 //Fix it so that the memberNumber increments correctly.
 
 
@@ -14,15 +14,15 @@ public class Member {
     private String address;
     private int phoneNumber;
     private int memberNumber;
-    private static int memberCounter = 0;
+    static int memberCounter = 0;
     private Enum<MembershipType> memberShipStatus;
     private Enum<MembershipType> membershipType;
     private Enum<MembershipType> ageGroup;
-
     private boolean hasPaid;
     private LocalDate ld;
 
 
+    //-------------------Constructor for new members--------------------------------------------------------------------
     public Member(String firstName, String lastName, LocalDate birthday, String gender, String address, int phoneNumber,
                   String membershipStatus, String membershipType, boolean hasPaid) {
         this.firstName = firstName;
@@ -36,10 +36,34 @@ public class Member {
         this.membershipType = MembershipType.membershipType(membershipType);
         this.ageGroup = MembershipType.ageGroup(age);
         this.hasPaid = hasPaid;
-        this.memberNumber = ++memberCounter;
+        this.memberNumber = ++memberCounter; // Increment for new members
         this.ld = birthday;
     }
 
+    //-------------------Constructor for members loaded from file-------------------------------------------------------
+    public Member(String firstName, String lastName, LocalDate birthday, String gender, String address, int phoneNumber,
+                  String membershipStatus, String membershipType, boolean hasPaid, int memberNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.name = lastName + "," + firstName;
+        this.age = calculateAge(birthday);
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.memberShipStatus = MembershipType.membershipStatus(membershipStatus);
+        this.membershipType = MembershipType.membershipType(membershipType);
+        this.ageGroup = MembershipType.ageGroup(age);
+        this.hasPaid = hasPaid;
+        this.memberNumber = memberNumber; // Use the number from the file
+        this.ld = birthday;
+
+        // Update memberCounter to ensure it stays ahead
+        if (memberNumber > memberCounter) {
+            memberCounter = memberNumber;
+        }
+    }
+
+    //-------------------Method to calculate age------------------------------------------------------------------------
     //Calculates the age based on the birthday provided and subtracts it from the current date.
     //Return year in between.
     public int calculateAge(LocalDate birthday) {
@@ -48,7 +72,7 @@ public class Member {
         return period.getYears();
     }
 
-//--------------------Method to determine the annual membership fee-----------------------------------------------------
+    //--------------------Method to determine the annual membership fee-----------------------------------------------------
     public int determineMembershipFee() {
         int fee = 0;
         int age = calculateAge(ld);
@@ -68,17 +92,24 @@ public class Member {
             } else {
                 fee = 500;
             }
-
         }
         return fee;
     }
 
-
-
     //-------------------Converts the member details into a CSV-style string--------------------------------------------
     public String toCSVStyle() {
-        return name + "," + ld.getYear() + "," + ld.getMonthValue() + "," + ld.getDayOfMonth() + "," + gender + "," + address + "," + phoneNumber +
-                "," + memberShipStatus + "," + membershipType + "," + ageGroup + "," + hasPaid + "," + memberNumber + "\n";
+        return name + "," +
+                ld.getYear() + "," +
+                ld.getMonthValue() + "," +
+                ld.getDayOfMonth() + "," +
+                gender + "," +
+                address + "," +
+                phoneNumber + "," +
+                memberShipStatus + "," +
+                membershipType + "," +
+                ageGroup + "," +
+                hasPaid + "," +
+                memberNumber + "\n";
     }
 
     //-------------------Formats the member details as a user-friendly string-------------------------------------------
@@ -104,9 +135,11 @@ public class Member {
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public String getName() {
         return name;
     }
