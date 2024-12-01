c13bd7cfa1451b1c2ed238de93feeac2a1b6542b
nial0003 <nial0003@stud.kea.dk>
2024-12-01 20:32:44 +0100

    Added the ability to add training times and update said value. Added the day the training was logged into the system to keep track.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.startProgram();
+        Trainer trainer = new Trainer("nikolaj");
+        Chairman chairman = new Chairman();
+        FileHandler fh = new FileHandler();
+
+        trainer.addAthletesToList(chairman.getListOfMembers());
+        trainer.saveAthleteMembersToAthleteFile();
+
+        ArrayList<String> athletesFromFile = fh.getAthletesFromAthleteFile();
+        ArrayList<String> updatedAthletesFromFile = trainer.setAthleteTrainingTime("Nana", athletesFromFile, "CRAWL", 42.0);
+        updatedAthletesFromFile = trainer.setAthleteTrainingTime("Nana", athletesFromFile, "brystsvømning", 55.0);
+        fh.saveUpdatedAthletesToFile(updatedAthletesFromFile);
     }
 }
