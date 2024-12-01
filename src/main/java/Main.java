e59e8c5f1e8345dbc639a17d6efe8dbd479dd416
nial0003 <nial0003@stud.kea.dk>
2024-12-01 20:38:28 +0100

    fucked up the main, should be fixed now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Trainer trainer = new Trainer("nikolaj");
-        Chairman chairman = new Chairman();
-        FileHandler fh = new FileHandler();
-
-        trainer.addAthletesToList(chairman.getListOfMembers());
-        trainer.saveAthleteMembersToAthleteFile();
-
-        ArrayList<String> athletesFromFile = fh.getAthletesFromAthleteFile();
-        ArrayList<String> updatedAthletesFromFile = trainer.setAthleteTrainingTime("Nana", athletesFromFile, "CRAWL", 42.0);
-        updatedAthletesFromFile = trainer.setAthleteTrainingTime("Nana", athletesFromFile, "brystsvømning", 55.0);
-        fh.saveUpdatedAthletesToFile(updatedAthletesFromFile);
+        UserInterface ui = new UserInterface();
+        ui.startProgram();
     }
 }
